ede62343f9bb80bfea6316e05c83896fe35ab2e1
InZenFenix <108585945+inzenfenix@users.noreply.github.com>
2025-10-03 16:37:19 -0300

    added support for msi e13 summit A11MT

--- msi-ec.c
+++ msi-ec.c
@@ -54,6 +54,74 @@ static DEFINE_MUTEX(ec_set_bit_mutex);
 #define FM_BASIC_NAME		"basic"
 #define FM_ADVANCED_NAME	"advanced"
 
+/* CUSTOM MSI SUMMIT E13 EVO A11MT */
+static const char *ALLOWED_FW_SUMMIT_E13[] __initconst = {
+    "13P2EMS1.110",
+    NULL
+};
+
+static struct msi_ec_conf CONF_SUMMIT_E13 __initdata = {
+    .allowed_fw = ALLOWED_FW_SUMMIT_E13,
+    .charge_control_address = 0xd7,
+    .webcam = {
+        .address       = 0x2e,
+        .block_address = 0x2f,
+        .bit           = 1,
+    },
+    .fn_win_swap = {
+        .address = 0xe8,
+        .bit     = 4,
+        .invert  = false
+    },
+    .cooler_boost = {
+        .address = 0x98,
+        .bit     = 7,
+    },
+    .shift_mode = {
+        .address = 0xd2,
+        .modes = {
+            { SM_ECO_NAME,     0xc2 },
+            { SM_COMFORT_NAME, 0xc1 },
+            { SM_TURBO_NAME,   0xc0 },
+            MSI_EC_MODE_NULL
+        },
+    },
+    .super_battery = {
+        .address = 0xeb,
+        .mask    = 0x0f,
+    },
+    .fan_mode = {
+        .address = 0xd4,
+        .modes = {
+            { FM_AUTO_NAME,     0x0d },
+            { FM_SILENT_NAME,   0x1d },
+            { FM_ADVANCED_NAME, 0x4d },
+            MSI_EC_MODE_NULL
+        },
+    },
+    .cpu = {
+        .rt_temp_address      = 0x68,
+        .rt_fan_speed_address = 0x71,
+    },
+    .gpu = {
+        .rt_temp_address      = MSI_EC_ADDR_UNSUPP,
+        .rt_fan_speed_address = 0x89,
+    },
+    .leds = {
+        .micmute_led_address = 0x2c,
+        .mute_led_address    = 0x2d,
+        .bit                 = 1,
+    },
+    .kbd_bl = {
+		.bl_mode_address  = 0x2c,
+		.bl_modes         = { 0x00, 0x08 },
+		.max_mode         = 1,
+		.bl_state_address = 0xf3,
+		.state_base_value = 0x80,
+		.max_state        = 3,
+	},
+};
+
 /* **************** Gen 1 - WMI1 **************** */
 
 static const char *ALLOWED_FW_G1_0[] __initconst = {
@@ -1614,6 +1682,7 @@ static struct msi_ec_conf *CONFIGURATIONS[] __initdata = {
 	&CONF_G2_4,
 	&CONF_G2_6,
 	&CONF_G2_10,
+	&CONF_SUMMIT_E13,
 	NULL
 };
 
